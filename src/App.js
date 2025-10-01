b9e500bfe54d13c5db1bb9e6b7496669e4f678e4
Alexander <alexander.espinas@uncedu.ph>
2025-10-01 14:57:46 +0800

    3rd Commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Router basename="/alxndr-portfolio-web">
             <Header />
             <Routes>
-                <Route path="/home" element={<Home />} />
+                <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/contact" element={<Contact />} />
                 <Route path="/projects" element={<Projects />} />
