cff9315cc0f0a7c01032ef313c1cc51483c1e5c6
Alexander <alexander.espinas@uncedu.ph>
2025-10-01 14:49:01 +0800

    3rd Commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import About from "./Pages/About";
 import Contact from "./Pages/Contact";
 import Projects from "./Pages/Projects";
 import Technologies from "./Pages/Technologies";
+
 function App() {
-  return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/technologies" element={<Technologies />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router basename="/alxndr-portfolio-web">
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/technologies" element={<Technologies />} />
+            </Routes>
+        </Router>
+    );
 }
+
 export default App;
